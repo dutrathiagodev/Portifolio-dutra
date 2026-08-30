26b07cee4a45a286b40ceb0c163e3a57a28487f2
dutrathiago <thiagodutrasap@gmail.com>
2026-08-29 23:02:51 -0300

    refactor: unifica destaque de menu ativo em js/nav.js e deixa contato.js so com a logica do form

--- contato/contato.js
+++ contato/contato.js
@@ -1,15 +1,3 @@
-const currentPage = window.location.pathname;
-
-const navLinks = document.querySelectorAll(".nav-links a");
-
-navLinks.forEach((link) => {
-  const linkPath = new URL(link.href).pathname;
-
-  if (currentPage.endsWith(linkPath)) {
-    link.classList.add("active");
-  }
-});
-
 // site nao tem backend, entao o "enviar" so monta um email pronto
 const form = document.getElementById("contact-form");
 
